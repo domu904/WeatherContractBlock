b2aea7ffb4a109f927fab4af1fd8933726cdd3f0
domu904 <domu904@gmail.com>
2016-09-05 00:04:14 -0500

    adding functionality for the weather contract

--- contracts/Weather.sol
+++ contracts/Weather.sol
@@ -1,5 +1,32 @@
 contract Weather {
   address public owner;
+  mapping (address => uint) balances;
 
-  
+
+  // Constructor
+  function Weather(){
+    owner = msg.sender;
+    balances[owner] = 1000;
+  }
+
+  function remove() {
+    if (msg.sender == owner){
+        selfdestruct(owner);
+    }
+  }
+
+  function putABet(address _betPlayer, uint _value) returns (bool success) {
+    //need to ensure the bet player put in the same amount as the owner of the contract
+    if(balances[msg.sender] < _value) {
+      return false;
+    }
+
+    balances[_betPlayer] += _value;
+    return true;
+  }
+
+  function figureOutWinner() returns (bool success) {
+    
+  }
+
 }
