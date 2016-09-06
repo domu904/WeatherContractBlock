b6c2b1c7f93715e877f63bdb0297e89db2053535
domu904 <domu904@gmail.com>
2016-09-05 20:16:48 -0500

    weather bet logic

--- contracts/Weather.sol
+++ contracts/Weather.sol
@@ -1,6 +1,7 @@
 contract Weather {
   address public owner;
   mapping (address => uint) balances;
+  uint weatherBet;
 
 
   // Constructor
@@ -15,18 +16,19 @@ contract Weather {
     }
   }
 
-  function putABet(address _betPlayer, uint _value) returns (bool success) {
+  function putABet(address _betPlayer, uint _ethValue, uint _weatherBet) returns (bool success) {
     //need to ensure the bet player put in the same amount as the owner of the contract
-    if(balances[msg.sender] < _value) {
+    if(balances[owner] < _ethValue) {
       return false;
     }
 
-    balances[_betPlayer] += _value;
+    balances[_betPlayer] += _ethValue;
+    weatherBet = _weatherBet;
     return true;
   }
 
   function figureOutWinner() returns (bool success) {
-    
+
   }
 
 }
